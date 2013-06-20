7e9d89d35934d48d53ca135b8a127ae96e33e04a
Lionel Martin <renarddesmers@gmail.com>
2013-06-21 00:10:38 +0200

    fixed oh my zsh link path

--- setup_env.sh
+++ setup_env.sh
@@ -19,7 +19,7 @@ curl http://j.mp/spf13-vim3 -L -o - | sh
 #echo "installing bash it"
 #git clone http://github.com/revans/bash-it.git ~/.bash_it && ~/.bash_it/install.sh
 echo "installing oh my zsh"
-ln -s ~/.homesick/repos/oh-my-zsh .oh-my-zsh
+ln -s ~/.homesick/repos/oh-my-zsh ~/.oh-my-zsh
 chsh -s /bin/zsh
 #echo "installing Liquid prompt"
 #git clone https://github.com/nojhan/liquidprompt.git ~/.liquidprompt
