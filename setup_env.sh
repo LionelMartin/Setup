f7dc50264e1ba861bad1144a40cdc771da29dc6c
Lionel Martin <lionel@lionelmartin.eu>
2019-06-20 15:12:20 +0200

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-aptget='sudo apt-get'
-chsh='sudo chsh'
-fccache='sudo fc-cache'
+aptget='sudo apt'
+
 if [ `whoami` = 'root' ]; then
-    aptget='apt-get'
-    chsh='chsh'
-    fccache='fc-cache'
+    aptget='apt'
+
 fi
 $aptget update
-$aptget install -y zsh git neovim tmux tmuxinator
+$aptget install -y zsh git neovim vim tmux tmuxinator
 
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 alias homeshick="$HOME/.homesick/repos/homeshick/home/.homeshick"
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 homeshick clone LionelMartin/Setup
-# homeshick clone robbyrussell/oh-my-zsh
-# git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
-# echo "installing oh my zsh"
-# ln -s ~/.homesick/repos/oh-my-zsh ~/.oh-my-zsh
-$chsh -s /bin/zsh
-$fccache -f -v
+chsh -s /bin/zsh
+fc-cache -f -v
