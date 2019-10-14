052c6b67f7f07b651e814e803d5cc8493355ebdd
Lionel Martin <lionel@lionelmartin.eu>
2019-10-14 15:23:41 -0400

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -4,15 +4,20 @@ aptget='sudo apt'
 
 if [ `whoami` = 'root' ]; then
     aptget='apt'
-
 fi
+
 $aptget update
-$aptget install -y zsh git neovim vim tmux tmuxinator
+$aptget install -y zsh git neovim vim tmux tmuxinator xclip
 
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 alias homeshick="$HOME/.homesick/repos/homeshick/home/.homeshick"
+
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 homeshick clone LionelMartin/Setup
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
+    vim +PlugInstall +qall
 
 chsh -s /bin/zsh
 fc-cache -f -v
